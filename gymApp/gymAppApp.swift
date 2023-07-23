8a9521ae121a5896376543767d9a387afc3f6485
alexltyt <alexltytca@gmail.com>
2023-07-23 17:42:45 -0600

    Initial Commit

---
+++ gymApp/gymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymAppApp.swift
+//  gymApp
+//
+//  Created by Alex Leung on 2023-07-23.
+//
+
+import SwiftUI
+
+@main
+struct gymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
