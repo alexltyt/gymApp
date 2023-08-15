7648ffacc3ab7b6f29d97d0990fdcfd6e0d32767
Greg Wu <wkl761212@hotmail.com>
2023-08-15 14:18:55 -0600

    update

---
+++ gymApp/Calendar.swift
@@ -0,0 +1,46 @@
+//
+//  Calendar.swift
+//  gymApp
+//
+//  Created by Greg on 2023-08-15.
+//
+
+
+import SwiftUI
+
+struct Calander: View {
+    @State private var title: String = ""
+    @State private var dueDate: Date = Date()
+
+    var body: some View {
+        VStack {
+            Text("New Workout")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 100)
+
+            Form {
+                // Title
+                TextField("Title", text: $title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+
+                // Due date
+                DatePicker("Due Date", selection: $dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+
+                // Button create
+                Button("Create") {
+                    // Implement your create logic here
+                }
+            }
+        }
+    }
+}
+
+struct Calander_Previews: PreviewProvider {
+    static var previews: some View {
+        Calander()
+    }
+}
+
+
